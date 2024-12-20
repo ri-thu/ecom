062836e475a290032567c65f57462d3985489b08
@ri-thu! <rhithikampradeep@gmail.com>
2024-12-20 15:35:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from "./ui/navbar/navbar.component";
+import { CardsComponent } from "./ui/cards/cards.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit  {
   title = 'ecom';
+
+  ngOnInit(): void {
+      initFlowbite();
+  }
 }
